6aba866db4c5a1b8815fa112292aab7996d77f7b
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-29 23:11:04 +0530

    test: crypto

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,57 @@
+import Block from "./blockchain/Block";
 import Chain from "./blockchain/Chain";
+import Transaction from "./blockchain/Transaction";
 import Wallet from "./blockchain/Wallet";
+import sshpk from 'sshpk';
+import key from './key.json';
 
+
+// const d = new Wallet();
+// const bob = new Wallet();
+// const chad = new Wallet();
+// // bob.publicKey = d.publicKey;
+
+// // bob.send(5000, chad.publicKey);
+// chad.send(1000, bob.publicKey);
 
 // console.log(Chain.getChain())
-const bob = new Wallet();
+// console.log('bob >>', Chain.getBalanceFromKey(bob.publicKey));
+// // console.log(sshpk.parseKey(bob.publicKey).toString(''));
+// // console.log(bob.publicKey == sshpk.parseKey(key.key, 'ssh').toString('pem'));
+// const hex = Buffer.from(bob.publicKey).toString('hex');
+// console.log('hex >>', hex);
+// console.log(sshpk.parseKey(Buffer.from(hex, 'hex')).toString('pem') === bob.publicKey);
+
+import express from 'express';
+import Server from './modules/Server';
+import cors from 'cors';
+import fs from 'fs';
+import multer from 'multer';
+
+const app = express();
+const PORT = Number(process.env.PORT) || 5000;
+const includeMulter = multer().any();
+
+
+const my = new Wallet();
 const chad = new Wallet();
 
-bob.send(5000, chad.publicKey);
-chad.send(1000, bob.publicKey);
+fs.writeFileSync('./public.pem', my.publicKey);
+fs.writeFileSync('./private.pem', my.privateKey);
+fs.writeFileSync('./tosend.pem', chad.publicKey);
 
-console.log(Chain.getChain())
-// Chain.getChain()[0] = 1;
-// console.log(Chain.getChain())
+app.use(express.json())
+app.use(cors());
 
-// console.log(bob.publicKey, chad.publicKey)
+app.use((req, res, next) => {
+    includeMulter(req, res, next)
+});
+
+
+app.use(express.static('public'));
+
+new Server(app, {
+    PORT: PORT,
+    methodsPath: './methods',
+    autoHandle: true
+});
